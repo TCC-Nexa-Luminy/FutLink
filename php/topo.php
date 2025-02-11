d53f094924d8b4ef035708244e9969fb74efbcd2
Shandel-dev <shandelvm16@gmail.com>
2025-02-11 14:30:51 -0300

    Upload inicial
    Upload do projeto FutLink com a estrutura de arquivos inicialmente definida

---
+++ php/topo.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FutLink</title>
+    <link rel="stylesheet" href="css/root.css">
+    <link rel="shortcut icon" href="imagens/futlinkLogoBg.png" type="image/x-icon">
+</head>
