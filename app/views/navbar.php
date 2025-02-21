6c27f736b8d2e943bf8c97fd1347d988a4ac007d
DanielHMF <shandelvm16@gmail.com>
2025-02-21 15:58:31 -0300

    testes
    Realizando testes de chamada de arquivos na nova estrutura de pastas

---
+++ app/views/navbar.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="../../public/css/navbar.css">
+
+<h2>Barra de navegação</h2>
--- public/index.php
+++ public/index.php
@@ -6,5 +6,24 @@ include("../app/views/topo.php");
 <body>
     <?php include("../app/views/navbar.php");?>
     <h1>Página inicial do projeto FutLink</h1>
+
+    <ul>
+        <li>Descriçao</li>
+        <li>Participantes</li>
+        <li>Wireframe</li>
+        <li>Tecnologias utilizadas</li>
+        <li>Ferramentas</li>
+        <li>Estrutura do projeto</li>
+        <li>Instalação</li>
+        <li>Uso</li>
+        <li>Licença</li>
+        <li>Agradecimentos</li>
+        - 👤 [Daniel Mattos](https://github.com/DanielHMF)
+- 👤 [Eduardo Fernandes](https://github.com/edudausp)
+- 👤 [Thiago Ribeiro](https://github.com/thiagoConsta)
+- 👤 [Murilo Magalhães](https://github.com/murimbo)
+- 👤 [Pedro Medeiros](https://github.com/phmsantostts)
+- 👤 [Shandel Villasante](https://github.com/Shandel-dev)
+    </ul>
 </body>
 </html>
