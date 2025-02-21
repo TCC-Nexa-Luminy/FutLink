9661bd9bd85169f6bef5820aaed746e774eb2d7a
DanielHMF <shandelvm16@gmail.com>
2025-02-21 15:07:17 -0300

    Estrutura de pastas
    Definição da Estrutura de pastas inicial do projeto do TCC

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php 
-include("php/topo.php");
+include("../app/views/topo.php");
 ?>
 <link rel="stylesheet" href="css/index.css">
 
 <body>
+    <?php include("../app/views/navbar.php");?>
     <h1>Página inicial do projeto FutLink</h1>
 </body>
 </html>
