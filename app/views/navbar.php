b099dc2c3b2342732b0bb2c60053c597ed771498
Jonas19-dev <shandelvm16@gmail.com>
2025-03-11 14:51:21 -0300

    FrontEnd Nav
    Definição do layout base da barra de navegação

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -1,12 +1,23 @@
 <link rel="stylesheet" href="../../public/css/navbar.css">
 
 <nav>
-    <div class="nav_logo">
-        <img src="" alt="">
-        <h1>FutLink</h1>
+    <div class="nav-container">
+        <img src="../public/images/futlinkLogoBg.png" alt="">
+        <ul class="nav-lista">
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre nós</a></li>
+            <li><a href="">Suporte</a></li>
+        </ul>
     </div>
-    <div class="nav_links">
-        <a href="">Sign In</a>
-        <a href="">Sign Up</a>
+    <div class="nav-container">
+        <input type="text" name="" id="" class="input-search" placeholder="Pesquise por times">
+    </div>
+    <div class="nav-container">
+            <a href="">Log In</a>
+            <a href="">Log Up</a>
+
+            <!-- Caso esteja logado, gerar as tags comentadas -->
+            <!-- <p>Bem vindo, Shandel! </p>
+            <img src="" alt="" class="icon-perfil"> -->
     </div>
 </nav>
