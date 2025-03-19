ff676c17236e5e6cf8114b818df56cd8934ec820
Shandel-dev <shandelvm16@gmail.com>
2025-03-19 17:46:35 -0300

    update cadastro

--- app/views/signUp.php
+++ app/views/signUp.php
@@ -7,61 +7,76 @@
             <a href="../../public/index.php" class="back_btn"><i class="fa-solid fa-circle-left"></i></a>
             <form action="../controllers/signUp.act.php" method="post" id="form_cadastro">
                 <h1>Cadastro</h1>
-                <p>Já tem uma conta? <a href="">Entre aqui</a></p>
+                <h2>Já tem uma conta? <a href="">Entre aqui</a></h2>
                 <label for="iprofile">
                     <input type="file" name="user_photo" id="iprofile" style="display: none;">
                     Foto de Perfil
                 </label>
+
+                <hr>
                 <article class="content-input">
-                    <p>Informações pessoais</p>
-                    <div class="form_label">
-                        <label for="inome">Nome</i></label>
-                        <input type="text" name="user_nome" id="inome" required placeholder="Nome Completo" autocomplete="off" class="input_user">
-                    </div>                    
+                    <h3>Informações pessoais</h3>
+                    <div class="form-label_direction">
+
+                        <div class="form-label">
+                            <label for="inome">Nome</i></label>
+                            <input type="text" name="user_nome" id="inome" required placeholder="Insira seu nome completo" autocomplete="off" class="input_user">
+                        </div>
+                        <div class="form-label">
+                            <label for="idate">Data de Nascimento</i></label>
+                            <input type="date" name="user_data_nasc" id="idate" required class="input_user">
+                        </div>
+                    </div>
+
+                    <fieldset>
+                        <legend><i class="fa-solid fa-venus-mars"></i> Gênero:</legend>
+                        <input type="radio" name="genero" id="imasc" value="masculino" checked>
+                        <label for="imasc">Masculino</label>
+
+                        <input type="radio" name="genero" id="ifem" value="feminino">
+                        <label for="ifem">Feminino</label>
+
+                        <input type="radio" name="genero" id="ioutro" value="outro">
+                        <label for="ioutro">Outro</label>
+
+                        <input type="radio" name="genero" id="iprefiro" value="prefiro não dizer">
+                        <label for="iprefiro">Prefiro não dizer</label>
+                    </fieldset>
                 </article>
+
+                <hr>
                 <article class="content-input">
-                    <p>Contato</p>
-                    <div class="form_label">
-                        <label for="iemail">Email</label>
-                        <input type="email" name="user_email" id="iemail" required placeholder="seuemail@email.com" autocomplete="off" class="input_user">
-                    </div>
-                    <div class="form_label">
-                        <label for="itel">Telefone</label>
-                        <input type="tel" name="user_tel" id="itel">
+                    <h3>Contato</h3>
+                    <div class="form-label_direction">
+
+                        <div class="form-label">
+                            <label for="iemail">Email</label>
+                            <input type="email" name="user_email" id="iemail" required placeholder="seuemail@email.com" autocomplete="off" class="input_user">
+                        </div>
+                        <div class="form-label">
+                            <label for="itel">Telefone</label>
+                            <input type="tel" name="user_tel" id="itel" placeholder="(xx)12345-6789">
+                        </div>
                     </div>
                 </article>
+
+                <hr>
                 <article class="content-input">
-                    <p>Segurança</p>
-                    <div class="form_label">
-
-                        <label for="ipass">Senha</label>
-                        <input type="password" name="user_pass" id="ipass" required placeholder="Insira sua senha" autocomplete="off" class="input_user">
-                    </div>
-                    <div class="form_label">
-                        <label for="ipass2">Confirme sua senha</i></label>
-                        <input type="password" name="user_pass2" id="ipass2" required placeholder="Confirme sua senha" autocomplete="off" class="input_user">
+                    <h3>Segurança</h3>
+                    <div class="form-label_direction">
+                        <div class="form-label">
+                            <label for="ipass">Senha</label>
+                            <input type="password" name="user_pass" id="ipass" required placeholder="Insira sua senha" autocomplete="off" class="input_user">
+                        </div>
+                        <div class="form-label">
+                            <label for="ipass2">Confirme sua senha</i></label>
+                            <input type="password" name="user_pass2" id="ipass2" required placeholder="Confirme sua senha" autocomplete="off" class="input_user">
+                        </div>
                     </div>
                 </article>
-                <div>
 
-                </div>
 
-                <input type="date" name="user_data_nasc" id="" required class="input_user">
 
-                <fieldset>
-                    <legend><i class="fa-solid fa-venus-mars"></i> Gênero:</legend>
-                    <input type="radio" name="genero" id="imasc" value="masculino" checked>
-                    <label for="imasc">Masculino</label>
-
-                    <input type="radio" name="genero" id="ifem" value="feminino">
-                    <label for="ifem">Feminino</label>
-
-                    <input type="radio" name="genero" id="ioutro" value="outro">
-                    <label for="ioutro">Outro</label>
-
-                    <input type="radio" name="genero" id="iprefiro" value="prefiro não dizer">
-                    <label for="iprefiro">Prefiro não dizer</label>
-                </fieldset>
                 <input type="submit" value="Finalizar cadastro">
             </form>
         </section>
