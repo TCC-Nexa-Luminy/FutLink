d2ca653a442d100d7f5acc51b0f51daf4b86bc78
Thiago Ribeiro <resque124@gmail.com>
2025-03-24 01:46:54 -0300

    Criação tela perfil

---
+++ public/js/perfil.js
@@ -0,0 +1,17 @@
+function abrirModal(){
+    document.getElementById("modalFoto").style.display = "block";
+
+}
+
+function fecharModal(){
+    document.getElementById("modalFoto").style.display = "none";
+
+}
+
+function adicionarFoto(){
+    alert("Foto adicionada com sucesso!");
+}
+
+function ExcluirFoto(){
+    alert("Foto excluida com sucesso!")
+}
