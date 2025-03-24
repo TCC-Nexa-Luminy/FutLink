36d947058004ce48f148a913bea47a198fd4f700
Thiago Ribeiro <resque124@gmail.com>
2025-03-24 17:46:05 -0300

    Criação da tela home

---
+++ app/views/home.php
@@ -0,0 +1,38 @@
+<?php include("../views/topo.php") ?>
+<link rel="stylesheet" href="../../public/css/home.css">
+
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="home.php">
+                <img src="../../public/images/futlinkLogoBg.png" alt="">
+            </a>
+        </div>
+        <ul class="infos">
+            <li><a href="">Sobre nós</a></li>
+            <li><a href="">Serviços</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+        <div class="buttons">
+            <a href="">Login <i class="fa-solid fa-angle-down"></i></a>
+            <a href="">Cadastrar <i class="fa-solid fa-angle-down"></i></a>
+        </div>
+    </nav>
+
+    <div class="navbar-container">
+
+        <div class="h1-page">
+            <h1>Sua chance de <br> brilhar no futebol <br> começa aqui!</h1>
+        </div>
+        <div class="h2-page">
+            <p>Mostre seu jogo, conquiste <br>
+                oportunidades e faça história no futebol.</p>
+        </div>
+        <div class="button-se-cadastre">
+            <button type="submit">
+                Criar seu perfil <i class="fa-solid fa-angle-right"></i>
+            </button>
+        </div>
+
+    </div>
+</body>
