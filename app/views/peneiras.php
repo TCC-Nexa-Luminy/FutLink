8bc4a5d859c5013d500296cf80607bb1a9875e4e
thiagoConsta <167592184+thiagoConsta@users.noreply.github.com>
2025-04-25 17:53:43 -0300

    att das box

--- app/views/peneiras.php
+++ app/views/peneiras.php
@@ -1,8 +1,10 @@
 <?php include("topo.php") ?>
-<?php include("navbar-social.php") ?>
+<link rel="stylesheet" href="../../public/css/peneiras.css">
 
 <body>
-    <link rel="stylesheet" href="../../public/css/peneiras.css">
+    <?php
+    // include("navbar-social.php")
+    ?>
     <div class="painel">
         <h1>Inscreva-se nas <br> melhores peneiras <br>do Brasil</h1>
         <p>Conquiste seu espaço no <br> futebol com as oportunidades <br>mais próximas de você.</p>
@@ -14,4 +16,34 @@
             </button>
         </form>
     </div>
+
+    <div class="painel-box">
+
+
+        <div class="list-box">
+
+            <div class="container-box">
+                <div class="infos">
+                    <img src="../../public/images/corinthians_p000Pmj_widexl.jpeg" alt="">
+                </div>
+
+                <div class="textos">
+                    <h1>Peneira thiagao</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem sed sequi praesentium harum laudantium aut!</p>
+                    <div class="button-box">
+                        <button id="button-box"><span class="nav-text">Ver mais</span></button>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+
+
 </body>
