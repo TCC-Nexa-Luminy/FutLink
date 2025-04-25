b7c1c1ebcf35a812deafa7d615a0a8971e93af35
Thiago Ribeiro <resque124@gmail.com>
2025-04-24 23:05:51 -0300

    Criando a tela peneiras muito boa e legal

---
+++ app/views/peneiras.php
@@ -0,0 +1,17 @@
+<?php include("topo.php") ?>
+<?php include("navbar-social.php") ?>
+
+<body>
+    <link rel="stylesheet" href="../../public/css/peneiras.css">
+    <div class="painel">
+        <h1>Inscreva-se nas <br> melhores peneiras <br>do Brasil</h1>
+        <p>Conquiste seu espaço no <br> futebol com as oportunidades <br>mais próximas de você.</p>
+
+        <form class="barra-pesquisa" action="" method="get">
+            <input type="text" placeholder="Pesquisar" class="campo-pesquisa" required>
+            <button type="submit" class="btn-pesquisar">
+                <i class="fa-solid fa-search"></i>
+            </button>
+        </form>
+    </div>
+</body>
