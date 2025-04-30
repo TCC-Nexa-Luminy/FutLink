64a124cfe57b5d90fd9fa2f51166a3b5dd2eb1c2
thiagoConsta <167592184+thiagoConsta@users.noreply.github.com>
2025-04-30 17:47:03 -0300

    tela-peneira-time/thiago

--- app/views/peneiraTime.php
+++ app/views/peneiraTime.php
@@ -1,9 +1,22 @@
 <?php include("topo.php") ?>
-<?php
-include("navbar-social.php");
-?>
 <link rel="stylesheet" href="../../public/css/peneiraTime.css">
 
 <body>
+    <?php include("topo.php"); ?>
+    <link rel="stylesheet" href="../../public/css/perfil.css">
 
-</body>
+    <body>
+        <main>
+            <div class="painel-gradient">
+                <a href="../../public/index.php" class="back_btn"><i class="fa-solid fa-circle-left"></i></a>
+            </div>
+
+
+            <section class="section-formulario">
+                <div class="infos">
+                    <img src="../../public/images/icon_user_image.png" alt="Foto do usuário" id="imagemUsuario">
+                    <h2>Thiago Ribeiro Costa<br>email@example.com</h2>
+                </div>
+            </section>
+        </main>
+    </body>
