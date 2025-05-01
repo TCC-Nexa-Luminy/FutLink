4b08e1aee39eac18301c5d20c3ceced1a8e2e351
Thiago Ribeiro <resque124@gmail.com>
2025-05-01 15:11:22 -0300

    Tela peneiras / peneira

--- app/views/peneiraTime.php
+++ app/views/peneiraTime.php
@@ -8,14 +8,71 @@
     <body>
         <main>
             <div class="painel-gradient">
-                <a href="../../public/index.php" class="back_btn"><i class="fa-solid fa-circle-left"></i></a>
+            <a href="peneiras.php" class="back_btn"><i class="fa-solid fa-circle-left"></i></a>
             </div>
 
 
             <section class="section-formulario">
                 <div class="infos">
-                    <img src="../../public/images/icon_user_image.png" alt="Foto do usuário" id="imagemUsuario">
-                    <h2>Thiago Ribeiro Costa<br>email@example.com</h2>
+                    <div class="info-titule">
+                        <img src="../../public/images/icon_user_image.png" alt="Foto do usuário" id="imagemTime">
+                        <h1>Nome da peneira -Corinthians-</h1>
+                    </div>
+
+                    <div class="detalhes-evento">
+                        <h2>Peneira: -Nome do clube-</h2>
+
+                        <div class="biografia-time">
+                            <!-- detalhes do evento -->
+                            <p>Você sonha em vestir o manto alvinegro e fazer história no futebol brasileiro? Essa é a sua chance!
+                                O Sport Club Corinthians Paulista está em busca de novos talentos para integrar suas categorias de base. Se você tem paixão pelo futebol, disciplina e garra em campo, venha participar da peneira oficial do Corinthians 2025. Mostre do que é capaz e dê o primeiro passo rumo à carreira de atleta profissional!
+                                As vagas são limitadas. Prepare-se, inscreva-se e venha mostrar seu futebol no terrão do Timão!</p>
+                        </div>
+
+                        <div class="infos-local-inscricao">
+                            <div class="localizao">
+                                <p><strong>Localização:</strong> São Paulo - CT Joaquim Grava</p>
+                            </div>
+                            <div class="data-hora">
+                                <p><strong>Data e hora:</strong> 15 de Julho de 2025 - 14h</p>
+                            </div>
+                            <div class="inscricao">
+                                <p><strong>Inscrição:</strong> Gratuita</p> <!--gratuita, R$10,00, R$100,00, ETC -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="infos-adicionais">
+                        <div class="documentação-detalhes">
+                            <!-- campo pro empresario escrever qual a documentação necessario para sua peneira -->
+                            <h2>Documentação obrigatória:</h2>
+                            <p>Cópia do RG ou certidão de nascimento</p>
+                            <p>Autorização assinada pelos pais ou responsáveis</p>
+                            <p>Atestado médico de aptidão física (emitido nos últimos 6 meses)</p>
+                        </div>
+
+                        <div class="idade">
+                            <h2>Idade:</h2>
+                            <p>Garotos nascidos entre 2007 e 2011 (13 a 17 anos)</p>
+                        </div>
+                    </div>
+
+                    <div class="fotos-local">
+                        <div class="titule-photo">
+                            <h2>Fotos do local:</h2>
+                        </div>
+                        <div class="fotos-grid">
+                            <div class="foto-exemplo"></div>
+                            <div class="foto-exemplo"></div>
+                            <div class="foto-exemplo"></div>
+                        </div>
+                    </div>
+
+                    <div class="botao-inscrever">
+                        <button>Inscrever-se</button>
+                    </div>
+
+
                 </div>
             </section>
         </main>
