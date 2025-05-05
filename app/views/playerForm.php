d239b34bc55f51c845b670b9cfa4a4f53a38298f
Jonas19-dev <shandelvm16@gmail.com>
2025-05-05 14:58:30 -0300

    update

--- app/views/playerForm.php
+++ app/views/playerForm.php
@@ -10,33 +10,29 @@ include_once("topo.php");
     include_once("message.php");
     ?>
 <body>
-    <form action="../controllers/playerForm.act.php" method="post">
+    <main id="pageMain">
+
         <h1>Quer se candidatar a jogadores em nossos times registrados neste plataforma?</h1>
         <h2>Informe estes dados para se tornar um jogador aqui no FutLink</h2>
-
+        
         <p>Desenvolver o front-end desta página de formulario de jogador</p>
-
-        <input type="text" name="descrip" id="" placeholder="Sobre mim">
-
-        <label for="ipeso">Informe seu peso: </label>
-        <input type="number" name="peso" id="ipeso" placeholder="Ex: 70.5" step="0.01" min="0" max="500" required>
-
-        <label for="ialtura">Informe sua altura: </label>
-        <input type="number" name="altura" id="ialtura" placeholder="Ex: 1.75" step="0.01" min="0" max="3" required>
-
-        <input type="hidden" name="id_user" value="<?= $_SESSION['id']; ?>"> <!--input que retorna o id do usuario-->
-
-        <label for="iposicao">Qual a sua posição? </label>
-        <select name="posicao" id="iposicao" required>
-            <option value="">Seletione uma das opções</option>
-            <option value="atacante">Atacante</option>
-            <option value="meia">Meia</option>
-            <option value="lateral">Lateral</option>
-            <option value="zagueiro">Zagueiro</option>
-            <option value="goleiro">Goleiro</option>
-        </select>
-        <input type="submit" value="Enviar">
-    </form>
+        
+        <p>Este formulario deve pedir os seguintes dados do usuário para que se torne um jogador:</p>
+        <ul>
+            <li>peso</li>
+            <li>altura</li>
+            <li>descrição</li>
+            <li>posição</li>
+            <li>pé dominante</li>
+        </ul>
+        <p>abaixo são inputs meramente ilustrativos, eles podem ser removidos ou modificados pelo dev front-end</p>
+        <form action="../controllers/playerForm.act.php" method="post">
+            <input type="number" name="peso" id="" step="0,01" min='0' required placeholder="peso">
+            <input type="hidden" name="id_user" value="<?= $_SESSION['id']; ?>"> <!--input que retorna o id do usuario, ele deve estar dentro de um form-->
+            
+            <input type="submit" value="Enviar">
+        </form>
+    </main>
 </body>
 
 </html>
