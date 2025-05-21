6fb31101644b5a1050747c39157e3701c575d4d9
Jonas19-dev <shandelvm16@gmail.com>
2025-05-21 14:33:35 -0300

    update

--- public/js/perfilJogador.js
+++ public/js/perfilJogador.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.querySelector(".navbar");
-    dadosPerfil();
+  dadosPerfil();
+  const dados = 
 
   window.addEventListener("scroll", function () {
     if (window.scrollY > 100) {
@@ -12,17 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 async function dadosPerfil() {
-    $.ajax({
-        type: "POST",
-        url: "../../app/controllers/perfilJogador.act.php",
-        data: {nome: "Shandel"},       //dados a serem enviados ao backend
-        dataType: "text",           //tipo de dados que se espera, como texto ou JSON
-        success: function (response) {
-            console.log(response);
-        },
-        error: function (xhr, status, error) {
-            console.error("Erro na requisição:", error);
-        }
-    });
+  $.ajax({
+    type: "POST",
+    url: "../../app/controllers/perfilJogador.act.php",
+    data: { nome: "Shandel" }, //dados a serem enviados ao backend
+    dataType: "text", //tipo de dados que se espera, como texto ou JSON
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (xhr, status, error) {
+      console.error("Erro na requisição:", error);
+    },
+  });
 }
-
