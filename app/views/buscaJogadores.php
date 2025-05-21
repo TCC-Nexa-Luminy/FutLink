c7ff4033e879514978f18ca3d16e4d3f243611e9
JonasDev <167595360+DanielHMF@users.noreply.github.com>
2025-05-21 17:51:11 -0300

    Pequenos detalhes
    adicionei o navbar na pagina de busca jogadores

--- app/views/buscaJogadores.php
+++ app/views/buscaJogadores.php
@@ -5,8 +5,11 @@
     <link rel="stylesheet" href="../../public/css/buscaJogadores.css">
     <title>Buscar Jogadores</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+
 </head>
 <body>
+    <?php include 'navbar-social.php'; ?>
+
     <div class="container">
 
     <div class="barraPesquisa">
