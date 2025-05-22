98e1c6dccf340891ddd03fdb99ade598e5dab22b
edudausp <166921968+edudausp@users.noreply.github.com>
2025-05-21 21:21:24 -0300

    melhora-explorar

--- app/views/buscaJogadores.php
+++ app/views/buscaJogadores.php
@@ -1,22 +1,23 @@
-<?php include("../views/topo.php"); ?>
-
-<html lang="pt-br">
-<head>
-    <link rel="stylesheet" href="../../public/css/buscaJogadores.css">
+<?php include('topo.php'); ?>
     <title>Buscar Jogadores</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Montserrat:wght@600;700;800&display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="../../public/css/buscaJogador.css">
+<body>
 
+<?php include 'navbar-social.php'; ?>
 
     <div class="container">
+        <h1 class="titulo">Buscar Jogadores</h1>
 
-    <div class="barraPesquisa">
-        <div class="pesquisaContainer">
-            <form method="GET" action="../controllers/buscaJogadores.act.php">
-                <input type="text" name="apelido" placeholder="Buscar jogador por nome..." />
-                <button type="submit"><i class="fas fa-search"></i></button>
-            </form>
+        <div class="barraPesquisa">
+            <div class="pesquisaContainer">
+                <form method="GET" action="buscaJogadores.act.php">
+                    <input type="text" name="apelido" placeholder="Buscar jogador por nome..." />
+                    <button type="submit"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
         </div>
-    </div>
-
 
         <div class="conteudo">
             <div class="filtros">
@@ -25,13 +26,13 @@
                 <div class="filtro">
                     <label>Idade:</label>
                     <div class="faixa">
-                        <input type="number" placeholder="De" id="idadeMin" min=0>
-                        <input type="number" placeholder="Até" id="idadeMax" min=0>
+                        <input type="number" placeholder="De" id="idadeMin" min="0">
+                        <input type="number" placeholder="Até" id="idadeMax" min="0">
                     </div>
                 </div>
 
                 <div class="filtro">
-                    <label>Peso (kg):</label>
+                    <label>Peso</label>
                     <select id="peso">
                         <option value="">Selecione</option>
                         <option value="asc">Menor peso</option>
@@ -42,8 +43,8 @@
                 <div class="filtro">
                     <label>Altura (cm):</label>
                     <div class="faixa">
-                        <input type="number" placeholder="De" id="altura_min" min=0>
-                        <input type="number" placeholder="Até" id="altura_max" min=0>
+                        <input type="number" placeholder="De" id="altura_min" min="0">
+                        <input type="number" placeholder="Até" id="altura_max" min="0">
                     </div>
                 </div>
 
@@ -66,84 +67,130 @@
                         <button type="button">Atacante</button>
                     </div>
                 </div>
+
+                <button class="btnAplicarFiltros">Aplicar Filtros</button>
             </div>
 
             <div class="jogadorGrid">
                 <div class="jogadorCard">
-                    <img src="../../public/images/bambu.png" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">Robson Bambu</span>
-                        <span class="idade">Idade: 22</span>
+                    <div class="jogadorImagem">
+                        <img src="../../public/images/bambu.png" alt="Foto do jogador">
+                        <span class="posicaoBadge">Zagueiro</span>
+                        <div class="jogadorOverlay">
+                            <h3>Robson Bambu</h3>
+                            <p>Idade: 22 anos</p>
+                        </div>
+                    </div>
+                    <div class="jogadorInfo">
+                        <h3>Robson Bambu</h3>
+                        <p>Idade: 22 anos</p>
+                        <div class="jogadorAcoes">
+                            <a href="#" class="acaoBtn"><i class="fas fa-comment"></i> Contatar</a>
+                            <a href="#" class="acaoBtn"><i class="fas fa-user-plus"></i> Ver perfil</a>
+                        </div>
                     </div>
                 </div>
 
                 <div class="jogadorCard">
-                    <img src="../../public/images/felixTorres.png" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">Felix Torres</span>
-                        <span class="idade">Idade: 16</span>
+                    <div class="jogadorImagem">
+                        <img src="images/felixTorres.png" alt="Foto do jogador">
+                        <span class="posicaoBadge">Zagueiro</span>
+                        <div class="jogadorOverlay">
+                            <h3>Felix Torres</h3>
+                            <p>Idade: 16 anos</p>
+                        </div>
+                    </div>
+                    <div class="jogadorInfo">
+                        <h3>Felix Torres</h3>
+                        <p>Idade: 16 anos</p>
+                        <div class="jogadorAcoes">
+                            <a href="#" class="acaoBtn"><i class="fas fa-comment"></i> Contatar</a>
+                            <a href="#" class="acaoBtn"><i class="fas fa-user-plus"></i> Ver perfil</a>
+                        </div>
                     </div>
                 </div>
 
                 <div class="jogadorCard">
-                    <img src="../../public/images/luan.png" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">Rei da America 2017</span>
-                        <span class="idade">Idade: 22</span>
+                    <div class="jogadorImagem">
+                        <img src="images/luan.png" alt="Foto do jogador">
+                        <span class="posicaoBadge">Meia</span>
+                        <div class="jogadorOverlay">
+                            <h3>Rei da America 2017</h3>
+                            <p>Idade: 22 anos</p>
+                        </div>
+                    </div>
+                    <div class="jogadorInfo">
+                        <h3>Rei da America 2017</h3>
+                        <p>Idade: 22 anos</p>
+                        <div class="jogadorAcoes">
+                            <a href="#" class="acaoBtn"><i class="fas fa-comment"></i> Contatar</a>
+                            <a href="#" class="acaoBtn"><i class="fas fa-user-plus"></i> Ver perfil</a>
+                        </div>
                     </div>
                 </div>
 
                 <div class="jogadorCard">
-                    <img src="player-photo.jpg" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">João Silva</span>
-                        <span class="idade">Idade: 22</span>
+                    <div class="jogadorImagem">
+                        <img src="player-photo.jpg" alt="Foto do jogador">
+                        <span class="posicaoBadge">Atacante</span>
+                        <div class="jogadorOverlay">
+                            <h3>João Silva</h3>
+                            <p>Idade: 22 anos</p>
+                        </div>
+                    </div>
+                    <div class="jogadorInfo">
+                        <h3>João Silva</h3>
+                        <p>Idade: 22 anos</p>
+                        <div class="jogadorAcoes">
+                            <a href="#" class="acaoBtn"><i class="fas fa-comment"></i> Contatar</a>
+                            <a href="#" class="acaoBtn"><i class="fas fa-user-plus"></i> Ver perfil</a>
+                        </div>
                     </div>
                 </div>
 
                 <div class="jogadorCard">
-                    <img src="player-photo.jpg" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">João Silva</span>
-                        <span class="idade">Idade: 22</span>
+                    <div class="jogadorImagem">
+                        <img src="player-photo.jpg" alt="Foto do jogador">
+                        <span class="posicaoBadge">Goleiro</span>
+                        <div class="jogadorOverlay">
+                            <h3>Carlos Oliveira</h3>
+                            <p>Idade: 25 anos</p>
+                        </div>
+                    </div>
+                    <div class="jogadorInfo">
+                        <h3>Carlos Oliveira</h3>
+                        <p>Idade: 25 anos</p>
+                        <div class="jogadorAcoes">
+                            <a href="#" class="acaoBtn"><i class="fas fa-comment"></i> Contatar</a>
+                            <a href="#" class="acaoBtn"><i class="fas fa-user-plus"></i> Ver perfil</a>
+                        </div>
                     </div>
                 </div>
 
                 <div class="jogadorCard">
-                    <img src="player-photo.jpg" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">João Silva</span>
-                        <span class="idade">Idade: 22</span>
+                    <div class="jogadorImagem">
+                        <img src="player-photo.jpg" alt="Foto do jogador">
+                        <span class="posicaoBadge">Lateral</span>
+                        <div class="jogadorOverlay">
+                            <h3>Pedro Santos</h3>
+                            <p>Idade: 19 anos</p>
+                        </div>
+                    </div>
+                    <div class="jogadorInfo">
+                        <h3>Pedro Santos</h3>
+                        <p>Idade: 19 anos</p>
+                        <div class="jogadorAcoes">
+                            <a href="#" class="acaoBtn"><i class="fas fa-comment"></i> Contatar</a>
+                            <a href="#" class="acaoBtn"><i class="fas fa-user-plus"></i> Ver perfil</a>
+                        </div>
                     </div>
                 </div>
-
-                <div class="jogadorCard">
-                    <img src="player-photo.jpg" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">João Silva</span>
-                        <span class="idade">Idade: 22</span>
-                    </div>
-                </div>
-
-                <div class="jogadorCard">
-                    <img src="player-photo.jpg" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">João Silva</span>
-                        <span class="idade">Idade: 22</span>
-                    </div>
-                </div>
-
-                <div class="jogadorCard">
-                    <img src="player-photo.jpg" alt="Foto do jogador">
-                    <div class="nome-idade">
-                        <span class="nome">João Silva</span>
-                        <span class="idade">Idade: 22</span>
-                    </div>
-                </div>
-
             </div>
         </div>
+
+        <div class="carregarMais">
+            <button class="btnCarregarMais">Carregar mais jogadores</button>
+        </div>
     </div>
-    <?php include("footer.php"); ?>
 </body>
 </html>
