6189fd8c2c51ab2a7a4a2178fcd36493eccb9285
Jonas19-dev <shandelvm16@gmail.com>
2025-05-30 14:55:05 -0300

    update

--- public/js/perfilJogador.js
+++ public/js/perfilJogador.js
@@ -9,7 +9,7 @@ async function dadosPerfil() {
     data: { nome: "Shandel" }, //dados a serem enviados ao backend
     dataType: "json", //tipo de dados que se espera, como texto ou JSON
     success: function (response) {
-      // console.log(response)
+      console.log(response)
       addDados(response);
     },
     error: function (xhr, status, error) {
@@ -66,7 +66,7 @@ function calcularIdade(dataNasc){
   let idade = hoje.getFullYear() - nasc.getFullYear();
   const mes = hoje.getMonth() - nasc.getMonth();
 
-  if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
+  if (mes < 0 || (mes === 0 && hoje.getDate() < nasc.getDate())) {
     idade--;
   }
 
